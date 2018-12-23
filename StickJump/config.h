0c9e51a17e0d3b82b51849a7073167fb3edc2910
Corina Tudorache <Corina-Stefania@users.noreply.github.com>
2018-12-23 20:23:07 +0200

    Add files via upload

---
+++ StickJump/config.h
@@ -0,0 +1,7 @@
+#define BUTTON_RIGHT_PIN 13
+#define BUTTON_LEFT_PIN 2
+#define V0_PIN 9
+#define SIZE 8 
+
+enum {SETUP, PLAY, LOST} state = PLAY;
+enum {LEVEL1, LEVEL2, LEVEL3} platformLevel = LEVEL1;
